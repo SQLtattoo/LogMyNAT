ed496946badfa0993a7a47d67c3e25975a8933c9
Vassilis Ioannidis <ioannidisv@gmail.com>
2021-02-20 14:41:45 +0200

    first version completed

--- LogMyNATPubIP/Form1.Designer.cs
+++ LogMyNATPubIP/Form1.Designer.cs
@@ -33,15 +33,22 @@ namespace LogMyNATPubIP
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.mynotifyicon = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openLogFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrPubIP = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lblLastChange = new System.Windows.Forms.Label();
+            this.lblOpenLog = new System.Windows.Forms.LinkLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mynotifyicon
             // 
             this.mynotifyicon.Icon = ((System.Drawing.Icon)(resources.GetObject("mynotifyicon.Icon")));
-            this.mynotifyicon.Text = "notifyIcon1";
+            this.mynotifyicon.Text = "LogMyNAT by SQLtattoo";
             this.mynotifyicon.Visible = true;
             this.mynotifyicon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mynotifyicon_MouseDoubleClick);
             // 
@@ -49,34 +56,114 @@ namespace LogMyNATPubIP
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.showToolStripMenuItem,
+            this.openLogFileToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
-            this.exitToolStripMenuItem.Text = "E&xit";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 92);
             // 
             // showToolStripMenuItem
             // 
             this.showToolStripMenuItem.Name = "showToolStripMenuItem";
             this.showToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.showToolStripMenuItem.Text = "&Show";
+            this.showToolStripMenuItem.Text = "&Open";
+            // 
+            // openLogFileToolStripMenuItem
+            // 
+            this.openLogFileToolStripMenuItem.Name = "openLogFileToolStripMenuItem";
+            this.openLogFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openLogFileToolStripMenuItem.Text = "Vie&w log file";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ubuntu Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "My current public IP:";
+            // 
+            // lblCurrPubIP
+            // 
+            this.lblCurrPubIP.Font = new System.Drawing.Font("Ubuntu Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrPubIP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblCurrPubIP.Location = new System.Drawing.Point(285, 58);
+            this.lblCurrPubIP.Name = "lblCurrPubIP";
+            this.lblCurrPubIP.Size = new System.Drawing.Size(195, 25);
+            this.lblCurrPubIP.TabIndex = 2;
+            this.lblCurrPubIP.Text = "0.0.0.0";
+            this.lblCurrPubIP.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Ubuntu Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(190, 2);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(344, 16);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Get LogMyNAT @ GitHub licenced under MIT License";
+            // 
+            // lblLastChange
+            // 
+            this.lblLastChange.Font = new System.Drawing.Font("Ubuntu Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastChange.ForeColor = System.Drawing.Color.DimGray;
+            this.lblLastChange.Location = new System.Drawing.Point(68, 95);
+            this.lblLastChange.Name = "lblLastChange";
+            this.lblLastChange.Size = new System.Drawing.Size(412, 20);
+            this.lblLastChange.TabIndex = 5;
+            this.lblLastChange.Text = "last change occured @";
+            this.lblLastChange.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblOpenLog
+            // 
+            this.lblOpenLog.Font = new System.Drawing.Font("Ubuntu Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOpenLog.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblOpenLog.Location = new System.Drawing.Point(72, 169);
+            this.lblOpenLog.Name = "lblOpenLog";
+            this.lblOpenLog.Size = new System.Drawing.Size(408, 19);
+            this.lblOpenLog.TabIndex = 6;
+            this.lblOpenLog.TabStop = true;
+            this.lblOpenLog.Text = "Open log to view all entries";
+            this.lblOpenLog.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblOpenLog.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblOpenLog_LinkClicked);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 429);
+            this.ClientSize = new System.Drawing.Size(536, 197);
+            this.Controls.Add(this.lblOpenLog);
+            this.Controls.Add(this.lblLastChange);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblCurrPubIP);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Roboto Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(552, 236);
             this.Name = "Form1";
-            this.Text = "LogMyNATPubIP";
+            this.Text = "LogMyNAT by SQLtattoo";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +173,13 @@ namespace LogMyNATPubIP
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCurrPubIP;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label lblLastChange;
+        private System.Windows.Forms.LinkLabel lblOpenLog;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem openLogFileToolStripMenuItem;
     }
 }
 
--- LogMyNATPubIP/Form1.cs
+++ LogMyNATPubIP/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,20 +14,82 @@ namespace LogMyNATPubIP
 {
     public partial class Form1 : Form
     {
+        private bool allowVisible;
+        private bool allowClose;
+        private static string startUpIP;
+        private static string currentIP;
+        private static string[] lastLoggedIP;
+        private string logFile = "LogMyNAT.log";
+
         public Form1()
         {
             InitializeComponent();
+
             mynotifyicon.ContextMenuStrip = contextMenuStrip1;
             this.showToolStripMenuItem.Click += showToolStripMenuItem_Click;
+            this.openLogFileToolStripMenuItem.Click += openLogFileToolStripMenuItem_Click;
             this.exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            
+            startUpIP = GetPublicIpAddress();
+            currentIP = startUpIP;
+            mynotifyicon.Text = "LogMyNAT (the public IP now is: " + currentIP + ")";
+            mynotifyicon.ShowBalloonTip(1000, "LogMyNAT - Starting up...", "Current IP: " + currentIP,ToolTipIcon.Info);
+
+            lblCurrPubIP.Text = currentIP;
+
+            if (File.Exists(logFile))
+            {
+                var lastLine = File.ReadLines(logFile).Last();
+                lastLoggedIP = lastLine.Split(';');
+                if (lastLoggedIP.Count() > 0)
+                {
+                    if (lastLoggedIP[0] != currentIP)
+                    {
+                        using (StreamWriter w = File.AppendText(logFile))
+                        {
+                            Log(currentIP, w);
+                        }
+                    }
+                    else
+                    {
+                        lblLastChange.Text = "Last logged change occurred @ " + lastLoggedIP[1];
+                    }
+                    
+                }
+            }
+            else
+            {
+                using (StreamWriter w = File.AppendText(logFile))
+                {
+                    w.WriteLine("IP;Timestamp"); //first time created, add headers
+                    Log(currentIP, w);
+                }
+            }
+
+            timer1.Start();
         }
 
-        private bool allowVisible;     // ContextMenu's Show command used
-        private bool allowClose;       // ContextMenu's Exit command used
-        
+        private void CheckAndLog()
+        {
+            string probeIP = GetPublicIpAddress();
+
+            if (currentIP != probeIP)
+            {
+                currentIP = probeIP;
+                using (StreamWriter w = File.AppendText(logFile))
+                {
+                    Log(currentIP, w);
+                    lblCurrPubIP.Text = currentIP;
+                    lblLastChange.Text = "Last logged change occurred @ " + lastLoggedIP[1];
+                }
+                mynotifyicon.ShowBalloonTip(1000, "LogMyNAT - IP changed!", "New IP: " + currentIP, ToolTipIcon.Warning);
+            }
+            
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            //this.WindowState = FormWindowState.Minimized;
+            
         }
 
         private static string GetPublicIpAddress()
@@ -35,28 +98,47 @@ namespace LogMyNATPubIP
             return externalip;
         }
 
+        private void showLog()
+        {
+            //open log
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.WriteLine(logMessage + ";" + $"{DateTime.UtcNow.ToString("s") + "Z"}");
+        }
+
+        public static void DumpLog(StreamReader r)
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
         private void Form1_Resize(object sender, EventArgs e)
         {
-            if (FormWindowState.Minimized == this.WindowState)
-            {
-                mynotifyicon.Visible = true;
-                mynotifyicon.ShowBalloonTip(10,"I'm down here","Still running in the background",ToolTipIcon.Info);
-                this.Hide();
-            }
+            //if (FormWindowState.Minimized == this.WindowState)
+            //{
+            //    mynotifyicon.Visible = true;
+            //    mynotifyicon.ShowBalloonTip(10,"I'm down here","Still running in the background",ToolTipIcon.Info);
+            //    this.Hide();
+            //}
 
-            else if (FormWindowState.Normal == this.WindowState)
-            {
-                mynotifyicon.Visible = false;
-            }
+            //else if (FormWindowState.Normal == this.WindowState)
+            //{
+            //    mynotifyicon.Visible = false;
+            //}
         }
 
         private void mynotifyicon_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            {
-                mynotifyicon.Visible = false;
-                this.Show();
-                this.WindowState = FormWindowState.Normal;
-            }
+            //{
+            //    mynotifyicon.Visible = false;
+            //    this.Show();
+            //    this.WindowState = FormWindowState.Normal;
+            //}
         }
 
         protected override void SetVisibleCore(bool value)
@@ -83,12 +165,34 @@ namespace LogMyNATPubIP
         {
             allowVisible = true;
             Show();
+            //showLog();
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             allowClose = true;
             Application.Exit();
+        }
+
+        private void openLogFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            allowClose = true;
+            Application.Exit();
+        }
+
+        private void lblOpenLog_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            openLogFile();
+        }
+
+        private void openLogFile()
+        {
+            System.Diagnostics.Process.Start(logFile);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CheckAndLog();
         }
 
     }
